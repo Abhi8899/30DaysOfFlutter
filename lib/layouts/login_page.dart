4940adb71e0d575e02a09d0a7652a69eeb1855a5
Abhi8899 <abhiwadekar88@gmail.com>
2022-11-15 11:54:35 +0530

    Update login_page.dart

--- lib/layouts/login_page.dart
+++ lib/layouts/login_page.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors, duplicate_ignore
+
+import 'package:demo/utils/routes.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -6,8 +9,55 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+        color: Colors.white,
         child: Column(
-      children: [Image.asset("assets/images/login_img.png")],
-    ));
+          children: [
+            Image.asset(
+              "assets/images/login_img.png",
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    // ignore: prefer_const_constructors
+                    decoration: InputDecoration(
+                        hintText: "Enter username", labelText: "UserNmae"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    // ignore: prefer_const_constructors
+                    decoration: InputDecoration(
+                        hintText: "Enter Password", labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                    style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    child: Text("Login"),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
